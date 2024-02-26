47f1b464ac2c54b0098b5ae21bcdd6f5b010811d
Leonardo de Lira Siqueira <leonardo018.siqueira@hotmail.com>
2024-02-26 07:02:45 -0300

    Implementing Controller and Injection Dependencies

---
+++ eCommerce.API/Controllers/UsersController.cs
@@ -0,0 +1,62 @@
+﻿using eCommerce.API.Repositories;
+using eCommerce.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eCommerce.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepos _repos;
+
+        public UsersController(IUserRepos repos)
+        {
+            _repos = repos;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var userList = _repos.Get();
+            return Ok(userList);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var user = _repos.Get(id);
+
+            if(user == null)
+            {
+                return NotFound("Não encontrado.");
+            }
+            else
+            {
+                return Ok(user);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody]User user)
+        {
+            _repos.Add(user);
+            return Ok(user);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update([FromBody]User user, int id)
+        {
+            _repos.Update(user);
+            return Ok(user);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repos.Delete(id);
+            return Ok();
+        }
+    }
+}
---
+++ eCommerce.API/Repositories/UserRepos.cs
@@ -0,0 +1,31 @@
+﻿using eCommerce.Models;
+
+namespace eCommerce.API.Repositories
+{
+    public class UserRepos : IUserRepos
+    {
+        public static List<User> _db = new List<User>();
+
+        public List<User> Get()
+        {
+            return _db;
+        }
+        public User Get(int id)
+        {
+            return _db.Find(x => x.Id == id)!;
+        }
+        public void Add(User user)
+        {
+            _db.Add(user);
+        }
+        public void Delete(int id)
+        {
+            _db.Remove(Get(id));
+        }
+        public void Update(User user)
+        {
+            _db.Remove(Get(user.Id));
+            _db.Add(user);
+        }
+    }
+}
