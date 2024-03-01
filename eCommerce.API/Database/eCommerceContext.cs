05631ddddedd587e6c89addc63c0aea708da8a63
Leonardo de Lira Siqueira <leonardo018.siqueira@hotmail.com>
2024-03-01 17:44:41 -0300

    Inserting connection string

--- eCommerce.API/Database/eCommerceContext.cs
+++ eCommerce.API/Database/eCommerceContext.cs
@@ -8,6 +8,11 @@ namespace eCommerce.API.Database
         {
         }
 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=eCommerce;Integrated Security=True;");
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<SendAddress> SendAddresses { get; set; }
