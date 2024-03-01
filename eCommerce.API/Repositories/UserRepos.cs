c43f70f79a8974e11bf514f893610a79ccf98dcc
Leonardo de Lira Siqueira <leonardo018.siqueira@hotmail.com>
2024-03-01 18:31:44 -0300

    Inserting UserRepos db context

--- eCommerce.API/Repositories/UserRepos.cs
+++ eCommerce.API/Repositories/UserRepos.cs
@@ -1,31 +1,39 @@
-﻿using eCommerce.Models;
+﻿using eCommerce.API.Database;
+using eCommerce.Models;
 
 namespace eCommerce.API.Repositories
 {
     public class UserRepos : IUserRepos
     {
-        public static List<User> _db = new List<User>();
+        private readonly eCommerceContext _db;
+        public UserRepos(eCommerceContext db) 
+        {
+            _db = db;
+        }
 
         public List<User> Get()
         {
-            return _db;
+            return _db.Users.OrderBy(x => x.Id).ToList();
         }
         public User Get(int id)
         {
-            return _db.Find(x => x.Id == id)!;
+            return _db.Users.Find(id)!;
         }
         public void Add(User user)
         {
-            _db.Add(user);
+            //unit of works
+            _db.Users.Add(user);
+            _db.SaveChanges();
         }
         public void Delete(int id)
         {
-            _db.Remove(Get(id));
+            _db.Users.Remove(Get(id));
+            _db.SaveChanges();
         }
         public void Update(User user)
         {
-            _db.Remove(Get(user.Id));
-            _db.Add(user);
+            _db.Users.Update(user);
+            _db.SaveChanges();
         }
     }
 }
