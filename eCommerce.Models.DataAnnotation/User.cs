6bbc8a7fde660813016281b5ce281116a83a4c7e
Leonardo de Lira Siqueira <leonardo018.siqueira@hotmail.com>
2024-03-06 06:39:48 -0300

    Inserting DataAnnotations to User.cs

--- eCommerce.Models.DataAnnotation/User.cs
+++ eCommerce.Models.DataAnnotation/User.cs
@@ -1,23 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace eCommerce.Models
 {
+    [Table("TB_USUARIOS")]
     public class User
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Id { get; set; }
+
+        //[Key]
+        //[Column("COD")]
+        //public int Code { get; set; }
+
         public string Name { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string? Sex { get; set; }
+
+        [Column("REGISTRO_GERAL")]
         public string? RG {  get; set; }
+
         public string CPF { get; set; } = null!;
         public string? NomeMae { get; set; }
         public string? RegisterSituation { get; set; }
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Registration { get; set; }
+
+        [NotMapped]
         public DateTimeOffset RegisterDate { get; set; }
-        public Contact? contact { get; set; }
+
+        [ForeignKey("UsuarioId")]
+        public Contact? Contact { get; set; }
         public ICollection<SendAddress>? SendAddresses { get; set; }
         public ICollection<Department>? Departments { get; set; }
     }
